eb31f89db3658543a11223e46e680980965d9ae3
Luke Harby <lukeharby@Lukes-MacBook-Pro.local>
2025-01-21 20:34:24 +0000

    Add website public_html index

--- watcher.ts
+++ watcher.ts
@@ -1,9 +1,36 @@
 import { watch } from "fs";
+import html from 'bun-plugin-html';
 
 const watcher = watch(
-  import.meta.dir,
+  `${import.meta.dir}/src`,
   { recursive: true },
   (event, filename) => {
-    console.log(`Detected ${event} in ${filename}`);
-  },
+    Bun.build({
+      entrypoints: ['./src/index-tmp.html'],
+      outdir: './static',
+      // minify: true,
+      plugins: [
+          html({
+              minifyOptions: {
+                  minifyCSS: true,
+                  minifyJS: true,
+                  minifyHTML: false,
+                  removeComments: true
+              },
+              inline: { 
+                  css: true,
+                  js: true
+              },
+              suppressErrors: true
+          })
+      ],
+  });
+
+  console.log(`Detected ${event} in ${filename} (src)`);
+  }
 );
+
+process.on('SIGINT', () => {
+  watcher.close();
+  process.exit(0);
+});
